62b139d3d6f23fe74faf9d6fdcec85b94384e92a
Mahmoud Aidia <mahmoudaidia7@gmail.com>
2026-03-16 17:02:03 +0200

    Finished about page

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,11 +1,12 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
     <div className="flex border-primary-800 border">
-      <img
+      <Image
         src={image}
         alt={`Cabin ${name}`}
         className="flex-1 border-r border-primary-800"
@@ -44,8 +45,7 @@ function CabinCard({ cabin }) {
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
           <a
             href={`/cabins/${id}`}
-            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
-          >
+            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900">
             Details & reservation &rarr;
           </a>
         </div>
